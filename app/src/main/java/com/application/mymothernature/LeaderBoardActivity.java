540888ab9717492ad7f46be46d8026c8493016f4
eric60 <ericshi98@yahoo.com>
2018-12-11 15:33:47 -0500

    added leaderboard button

--- app/src/main/java/com/application/mymothernature/LeaderBoardActivity.java
+++ app/src/main/java/com/application/mymothernature/LeaderBoardActivity.java
@@ -1,5 +1,6 @@
 package com.application.mymothernature;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,12 +10,22 @@ import android.view.View;
 
 import com.example.ericshi.mymothernature.R;
 
+import ru.dimorinny.floatingtextbutton.FloatingTextButton;
+
 public class LeaderBoardActivity extends AppCompatActivity {
+    private FloatingTextButton button_viewProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
 
+        button_viewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
